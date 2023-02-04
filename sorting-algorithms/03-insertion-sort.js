0b6ec5b147337180ffb1a4a24698dac5b0a0e97f
pyaephyowinn <pyaephyowinn@gmail.com>
2023-02-05 00:48:56 +0630

    fix: insertion-sort

--- sorting-algorithms/03-insertion-sort.js
+++ sorting-algorithms/03-insertion-sort.js
@@ -1,18 +1,13 @@
 const insertionSort = (arr) => {
   for (let i = 1, len = arr.length; i < len; i++) {
     let selectedVal = arr[i];
-    for (j = i - 1; j >= 0 && selectedVal < arr[j]; j--) {
-      console.log(selectedVal, arr[j], selectedVal < arr[j]);
-      arr[i] = arr[j];
-      arr[j] = selectedVal;
+    for (var j = i - 1; j >= 0 && selectedVal < arr[j]; j--) {
+      arr[j + 1] = arr[j];
+      // arr[j] = selectedVal;
     }
+    arr[j + 1] = selectedVal;
   }
   return arr;
 };
 
-console.log(insertionSort([5, 1, 4, 2, 1, 11, 66, 3, 2, 19, 63, 52]));
-
-// 3, _, 2, 4 ;  1
-// 1, 3, _, 4 ;  2
-// 1, 2, 3, 4 ;  4
-// 1, 2, 3, 4 ;  4
+console.log(insertionSort([4, 1, 3, 2, 5, 44, -1]));
